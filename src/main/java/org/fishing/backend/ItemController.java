a0aa6882b1e667d23c5da4d88f180f96d7ac0abd
Choi_Sungsik <sungsik97@naver.com>
2024-09-30 16:00:41 +0900

    Initial commit

---
+++ src/main/java/org/fishing/backend/ItemController.java
@@ -0,0 +1,22 @@
+package org.fishing.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    @GetMapping("/api/items")
+    public List<String> getItems(){
+
+        List<String> items = new ArrayList<>();
+        items.add("원투");
+        items.add("민물");
+        items.add("루어");
+
+        return items;
+    }
+}
