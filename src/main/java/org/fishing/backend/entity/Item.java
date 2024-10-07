ccf7968cb397f217245e755541ad5d1e29fef93a
Choi_Sungsik <sungsik97@naver.com>
2024-10-07 18:04:31 +0900

    feat : jpa작업 1

---
+++ src/main/java/org/fishing/backend/entity/Item.java
@@ -0,0 +1,16 @@
+package org.fishing.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+}
---
+++ src/main/java/org/fishing/backend/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package org.fishing.backend.repository;
+
+import org.fishing.backend.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ItemRepository extends JpaRepository<Item, Integer> {
+
+}
