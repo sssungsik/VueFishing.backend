c0d6ea7b945255fa515eafdf92420583cf8a362b
Choi_Sungsik <sungsik97@naver.com>
2024-10-08 17:00:39 +0900

    feat : db 연동 / 사진 추가 / 카드 스타일 지정

--- src/main/java/org/fishing/backend/repository/ItemRepository.java
+++ src/main/java/org/fishing/backend/repository/ItemRepository.java
@@ -3,6 +3,6 @@ package org.fishing.backend.repository;
 import org.fishing.backend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ItemRepository extends JpaRepository<Item, Integer> {
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 }
